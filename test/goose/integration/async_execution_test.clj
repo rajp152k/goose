eb5a0a72f836e6d3fcee998ef4c95cc21e390fb1
Raj <rajp152k@gmail.com>
2026-03-19 22:18:36 +0530

    async_execution_test with temp namespace
     - goose.temp sentinel namespace created
     - intern symbols on the fly in that (temp-intern-function)
     - tests passing : `clj -X:test :nses '[goose.integration.async-execution-test]'`

--- test/goose/integration/async_execution_test.clj
+++ test/goose/integration/async_execution_test.clj
@@ -1,22 +1,23 @@
 (ns goose.integration.async-execution-test
   (:require
+   [goose.temp]
    [goose.client :as c]
    [goose.worker :as w]
    [goose.test-utils :as tu]
-   [clojure.test :refer [deftest is testing use-fixtures]]))
+   [clojure.test :refer [deftest is testing use-fixtures run-test]]))
 
 (use-fixtures :each tu/redis-fixture)
+
+(defn temp-intern-function [function-symbol function]
+  (symbol (intern 'goose.temp function-symbol function)))
 
 (deftest perform-async-test
   (testing "Goose executes a function asynchronously"
     (let [res (promise)
-          deliver-fn (let [test-ns 'goose.integration.async-execution-test.tmp.perform-async-test
-                           fn-sym 'deliver-result
-                           test-fn #(deliver res %)]
-                       (create-ns test-ns)
-                       (intern test-ns fn-sym test-fn)
-                       (symbol (str test-ns) (str fn-sym)))]
-      (c/perform-async tu/redis-client-opts deliver-fn "async-execute-test")
+          deliver-fn (temp-intern-function 'deliver-fn
+                                           #(deliver res %))]
+      (c/perform-async tu/redis-client-opts
+                       deliver-fn :package)
       (let [worker (w/start tu/redis-worker-opts)]
-        (is (= (deref res 1000 :timed-out) "async-execute-test"))
+        (is (= (deref res 100 :timed-out) :package))
         (w/stop worker)))))
