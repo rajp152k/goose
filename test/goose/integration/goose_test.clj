23d3ed0212f93bc422a0d6d83bd1d0c96e390017
Raj <rajp152k@gmail.com>
2026-05-07 12:24:40 +0530

    test: refactor : unified goose_test ns

--- test/goose/integration/goose_test.clj
+++ test/goose/integration/goose_test.clj
@@ -7,39 +7,43 @@
    [goose.worker :as w]
    [clojure.test :refer [is]])
   (:import
-   [java.time Instant]))
+   [java.time Instant]
+   [java.util UUID]))
 
 (def-integration-test async-execution-test
   #{:enqueue}
-  (let [_ (c/perform-async (u/get-opts broker :client)
-                           `executable
-                           test-name
-                           ::async-execution-test)
+  (let [job (c/perform-async (u/get-opts broker :client)
+                             `executable
+                             test-name
+                             ::async-execution-test)
         worker (w/start (u/get-opts broker :worker))]
+    (is (uuid? (UUID/fromString (:id job))))
     (is (= ::async-execution-test
            (delivered-execution test-name)))
     (w/stop worker)))
 
 (def-integration-test absolute-scheduling-test
   #{:schedule}
-  (let [_ (c/perform-at (u/get-opts broker :client)
-                        (Instant/now)
-                        `executable
-                        test-name
-                        ::absolute-scheduling-test)
+  (let [job (c/perform-at (u/get-opts broker :client)
+                          (Instant/now)
+                          `executable
+                          test-name
+                          ::absolute-scheduling-test)
         scheduler (w/start (u/get-opts broker :worker))]
+    (is (uuid? (UUID/fromString (:id job))))
     (is (= ::absolute-scheduling-test
            (delivered-execution test-name)))
     (w/stop scheduler)))
 
 (def-integration-test relative-scheduling-test
   #{:schedule}
-  (let [_ (c/perform-in-sec (u/get-opts broker :client)
-                            1
-                            `executable
-                            test-name
-                            ::relative-scheduling-test)
+  (let [job (c/perform-in-sec (u/get-opts broker :client)
+                              1
+                              `executable
+                              test-name
+                              ::relative-scheduling-test)
         scheduler (w/start (u/get-opts broker :worker))]
+    (is (uuid? (UUID/fromString (:id job))))
     (is (= ::relative-scheduling-test
            (delivered-execution test-name)))
     (w/stop scheduler)))
